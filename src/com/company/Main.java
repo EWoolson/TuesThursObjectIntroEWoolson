f0871edb56b3f5cb268d576628335693b2c09c0c
Evan <ewgaming38@gmail.com>
2021-09-21 10:33:03 -0400

    Working BankAccount

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
         var myAccount = new BankAccount();
         var yourAccount = new BankAccount(2000, 0.05f);
