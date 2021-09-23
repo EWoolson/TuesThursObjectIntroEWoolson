c17d3355cdc183f6538833bcd04e644cee496fc3
Evan <ewgaming38@gmail.com>
2021-09-23 11:17:10 -0400

    working menu

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -1,8 +1,56 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Bank {
     private ArrayList<BankAccount> allAccounts;
     private ArrayList<Customer> allCustomers;
+
+    public Bank(){
+        allAccounts = new ArrayList<BankAccount>();
+        allCustomers = new ArrayList<Customer>();
+    }
+
+    public void doBanking(){
+        var menuReader = new Scanner(System.in);
+        while(true){
+            printMenu();
+            var userChoice = menuReader.nextInt();
+            switch (userChoice){
+                case 1:
+                    System.exit(0);
+                case 2:
+                    addCustomer(menuReader);
+                    break;
+                case 3:
+                    Customer current = selectCustomer(menuReader);
+                    break;
+                default:
+                    System.out.println("Please choose one of the menu options");
+            }
+        }
+    }
+
+    private Customer selectCustomer(Scanner reader) {
+
+    }
+
+    private void addCustomer(Scanner inputReader) {
+        inputReader.nextLine(); //eat the orphan newline from previous nextInt call
+        System.out.print("What is the new customer's name: ");
+        var customerName = inputReader.nextLine();
+        System.out.print("What is the new Customer's Tax ID (SSN): ");
+        var taxID = inputReader.nextInt();
+        var newCustomer = new Customer(customerName, taxID);
+        allCustomers.add(newCustomer);
+    }
+
+    private void printMenu() {
+        System.out.println("What would you like to do next (Select the number): ");
+        System.out.println("   [1] Close Program");
+        System.out.println("   [2] Add Customer");
+        System.out.println("   [3] Select Customer by ID");
+        System.out.print("Enter choice: ");
+    }
 }
